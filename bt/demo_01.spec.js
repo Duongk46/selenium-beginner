bcc25b8aaf5d2f53dddc62608cef1acd13ee4567
Duongk46 <96983351+Duongk46@users.noreply.github.com>
2024-03-23 01:42:43 +0700

    up

--- bt/demo_01.spec.js
+++ bt/demo_01.spec.js
@@ -2,42 +2,54 @@
 const { By, Button, Browser, until, Builder, Key } = require('selenium-webdriver');
 const { suite, ignore } = require('selenium-webdriver/testing');
 const assert = require('assert');
-// (async function example() {
-//     let driver = await new Builder().forBrowser('chrome').build();
-//     try {
-//         await driver.get('https://github.com/login');
-//         await driver.findElement(By.id('login_field')).sendKeys("minhduong18072002@gmail.com");
-//         await driver.findElement(By.id('password')).sendKeys("minhduong180702d@D", Key.RETURN);
-//         await driver.wait(until.titleIs('GitHub'), 10000);
-//         assert.deepStrictEqual(await driver.getTitle(), `GitHub`)
 
-//     } finally {
-//         // Đóng trình duyệt
-//         await driver.quit();
-//     }
-// })();
 suite(function (env) {
     describe("Test login in page github", function () {
         let driver;
         before(async () => {
-            driver = await new Builder().forBrowser(Browser.CHROME).build();
+            driver = await env.builder().build();
         });
 
         after(async () => await driver.quit());
+        // ignore(env.browsers(Browser.FIREFOX, Browser.SAFARI)).it('Back click', async function () {
+        //     await driver.get('https://selenium.dev/selenium/web/mouse_interaction.html');
+        //     await driver.findElement(By.id("click")).click();
 
-        it("Test case 1", async function () {
-            await driver.get('https://github.com/login');
-            await driver.findElement(By.id('login_field')).sendKeys("minhduong18072002@gmail.com");
-            await driver.findElement(By.id('password')).sendKeys("minhduong180702d@D",);
-            await driver.wait(until.titleIs('GitHub'), 10000);
-            let pageTitle = await driver.getTitle();
-            if (pageTitle === 'Trang chủ - Example') {
-                console.log('Đăng nhập thành công!');
-            } else {
-                console.log('Đăng nhập thất bại!');
+        //     assert.deepStrictEqual(await driver.getTitle(), `We Arrive Here`)
+
+        //     const actions = driver.actions({ async: true });
+        //     await actions.press(Button.BACK).release(Button.BACK).perform()
+
+        //     assert.deepStrictEqual(await driver.getTitle(), `BasicMouseInterfaceTest`)
+        // });
+
+        // ignore(env.browsers(Browser.FIREFOX, Browser.SAFARI)).it('Forward click', async function () {
+        //     await driver.get('https://selenium.dev/selenium/web/mouse_interaction.html');
+        //     await driver.findElement(By.id("click")).click();
+        //     await driver.navigate().back();
+
+        //     assert.deepStrictEqual(await driver.getTitle(), `BasicMouseInterfaceTest`)
+
+        //     const actions = driver.actions({ async: true });
+        //     await actions.press(Button.FORWARD).release(Button.FORWARD).perform()
+
+        //     assert.deepStrictEqual(await driver.getTitle(), `We Arrive Here`)
+        // });
+        ignore(env.browsers(Browser.FIREFOX, Browser.SAFARI)).it("Test case 1", async function () {
+            try {
+                // driver = await new Builder().forBrowser(Browser.CHROME).build();
+                await driver.get('https://github.com/login');
+                let userName = await driver.findElement(By.id('login_field'));
+                let password = await driver.findElement(By.id('password'));
+                await userName.sendKeys("...");
+                await password.sendKeys("...", Key.RETURN);
+                await driver.wait(
+                    until.elementLocated(By.name('commit')),
+                    5000);
             }
-            // assert.deepStrictEqual(await driver.getTitle(), `GitHub`)
-            // await submitButton.click();
-        })
+            catch (ex) {
+                console.log(ex)
+            }
+        });
     })
 }, { browsers: [Browser.CHROME] })
